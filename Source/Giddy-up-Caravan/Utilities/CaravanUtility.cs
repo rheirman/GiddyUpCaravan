c7b75566a71cd568f746d7f320473a7f39658573
Roel <roelheirman@gmail.com>
2018-01-01 15:25:45 +0100

    cleaned unused code + added translations

--- Source/Giddy-up-Caravan/Utilities/CaravanUtility.cs
+++ Source/Giddy-up-Caravan/Utilities/CaravanUtility.cs
@@ -15,16 +15,6 @@ namespace GiddyUpCaravan.Utilities
             ExtendedPawnData pawndata = GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(pawn);
             if (pawn.IsColonist && pawndata != null && pawndata.caravanMount != null)
             {
-                //Log.Message("pawn is colonis, has pawndata and has caravan mount");
-                //Log.Message("Pawn is caravan member: " + pawn.IsCaravanMember());
-                //Log.Message("CaravanMount is caravan member: " + pawndata.caravanMount.IsCaravanMember());
-               // Log.Message("pawn has same caravan as mount: " + (pawn.GetCaravan() == pawndata.caravanMount.GetCaravan()));
-                //ExtendedPawnData mountData = GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(pawndata.mount);
-
-                // if (mountData.selectedForCaravan)
-                //{
-                //     return true;
-                //}
                 return true;
 
             }

e45f414378f1da7b1611d5b4ad40869020cd4da0
rheirman <roelheirman@gmail.com>
2020-04-05 13:34:50 +0200

    cleanup + fixed Save our ship 2 incompatibility

--- v1.1/Source/Giddy-up-Caravan/Harmony/Lordtoil_PrepareCaravan_Leave.cs
+++ v1.1/Source/Giddy-up-Caravan/Harmony/Lordtoil_PrepareCaravan_Leave.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using Verse;
 using Verse.AI;
+using Verse.AI.Group;
 
 namespace GiddyUpCaravan.Harmony
 {
@@ -16,10 +17,11 @@ namespace GiddyUpCaravan.Harmony
     {
         static void Prefix(LordToil_PrepareCaravan_Leave __instance)
         {
+            AddMissingPawnsToLord(__instance);
             foreach (Pawn pawn in __instance.lord.ownedPawns)
             {
                 ExtendedPawnData pawnData = GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(pawn);
-                if(pawnData.caravanMount != null && __instance.lord.ownedPawns.Contains(pawnData.caravanMount))
+                if (pawnData.caravanMount != null)
                 {
                     Pawn animal = pawnData.caravanMount;
                     Job jobRider = new Job(GUC_JobDefOf.Mount, animal);
@@ -31,6 +33,26 @@ namespace GiddyUpCaravan.Harmony
                     jobAnimal.count = 1;
                     animal.jobs.TryTakeOrderedJob(jobAnimal);
                 }
+            }
+        }
+
+        //For compatibility with other mods (Save our ship 2), add any missing mounts to the lord. 
+        private static void AddMissingPawnsToLord(LordToil_PrepareCaravan_Leave __instance)
+        {
+            List<Pawn> shouldAddOwnedPawns = new List<Pawn>();
+            foreach (Pawn pawn in __instance.lord.ownedPawns)
+            {
+                ExtendedPawnData pawnData = GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(pawn);
+                bool caravanContainsMount = __instance.lord.ownedPawns.Contains(pawnData.caravanMount);
+                if (pawnData.caravanMount != null && !caravanContainsMount)
+                {
+                    shouldAddOwnedPawns.Add(pawnData.caravanMount);
+                }
+            }
+            foreach (Pawn pawn in shouldAddOwnedPawns)
+            {
+                __instance.lord.ownedPawns.Add(pawn);
+                pawn.mindState.duty = new PawnDuty(DutyDefOf.TravelOrWait, Traverse.Create(__instance).Field("exitSpot").GetValue<IntVec3>());
             }
         }
     }
